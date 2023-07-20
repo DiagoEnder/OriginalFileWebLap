8b14c41fa56d9155f67b846c8e658da645ff868b
Quang <ACER@LAPTOP-FAANHK7K>
2023-07-20 12:25:18 +0700

    Admin

---
+++ OriginalFileWebLap/Areas/Admin/Controllers/DataanalysisController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OriginalFileWebLap.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class DataanalysisController : Controller
+    {
+        public IActionResult Dataanalysis2()
+        {
+            return View();
+        }
+    }
+}
