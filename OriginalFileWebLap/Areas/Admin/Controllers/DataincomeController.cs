12fb978e247ef01c4b1358f8a2a5ff282dbf7656
Quang <ACER@LAPTOP-FAANHK7K>
2023-07-25 15:46:28 +0700

    Quang - two layout data data analyst

---
+++ OriginalFileWebLap/Areas/Admin/Controllers/DataincomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OriginalFileWebLap.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class DataincomeController : Controller
+    {
+        public IActionResult Income1()
+        {
+            return View();
+        }
+
+        public IActionResult Income2()
+        {
+            return View();
+        }
+    }
+}
--- OriginalFileWebLap/Controllers/HomeController.cs
+++ OriginalFileWebLap/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 
 namespace OriginalFileWebLap.Controllers
 {
+    [Area("Admin")]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
