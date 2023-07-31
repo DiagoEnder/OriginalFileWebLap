b757a6fb0e9e63ecf313680a0af9d958c799136d
Quang <ACER@LAPTOP-FAANHK7K>
2023-07-31 19:36:25 +0700

    SQL

---
+++ OriginalFileWebLap/Models/CompanyContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace OriginalFileWebLap.Models
+{
+    public class CompanyContext : DbContext
+    {
+        public CompanyContext(DbContextOptions<CompanyContext> options) : base(options) { }
+        public DbSet<MayTinh> mayTinhs { get; set; }
+    }
+}
