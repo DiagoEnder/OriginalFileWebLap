d3bda0a2eb7948223d8614bb09b331e260b45526
DiagoEnder <139749219+DiagoEnder@users.noreply.github.com>
2023-08-01 21:44:17 +0700

    Delete CompanyContext.cs

--- OriginalFileWebLap/Models/CompanyContext.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.EntityFrameworkCore;
-namespace OriginalFileWebLap.Models
-{
-    public class CompanyContext : DbContext
-    {
-        public CompanyContext(DbContextOptions<CompanyContext> options) : base(options) { }
-        public DbSet<MayTinh> mayTinhs { get; set; }
-    }
-}
