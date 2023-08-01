ff764892f822885b18b879702cb3ec3c5defd8a3
Tien Dat <tiendatphr@gmail.com>
2023-08-01 22:21:21 +0700

    Update-FirstTime

---
+++ OriginalFileWebLap/Models/Comment.cs
@@ -0,0 +1,16 @@
+﻿namespace OriginalFileWebLap.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public int IdProduct { get; set; }
+        public string IdUser { get; set; }
+        public string NameUSer { get; set; }
+        public string Content { get; set; }
+        
+
+        public Product Product { get; set; }
+
+
+    }
+}
---
+++ OriginalFileWebLap/Models/Requirement.cs
@@ -0,0 +1,11 @@
+﻿namespace OriginalFileWebLap.Models
+{
+    public class Requirement
+    {
+        public int IdRe { get; set; }
+        [Required]
+        public string ReName { get; set; }
+
+        public ICollection<Product> products { get; set; }
+    }
+}
