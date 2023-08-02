a51c66b73a2d8cac3fed3212ba3ff12e8d80ce12
DiagoEnder <139749219+DiagoEnder@users.noreply.github.com>
2023-08-02 13:13:48 +0700

    Delete OriginalFileWebLap/Models directory

--- OriginalFileWebLap/Models/AdminAccount.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace OriginalFileWebLap.Models
-{
-    public class AdminAccount
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Password { get; set; }
-    }
-}
--- OriginalFileWebLap/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace OriginalFileWebLap.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
